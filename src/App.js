b63a53962a25e208f7196903bd5e89d6976afb26
leilaybnz <leilanybanez@gmail.com>
2018-05-21 16:58:58 -0300

    Making maim

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-main">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="App-title">About</h1>
+        </div>
       </div>
     );
   }
